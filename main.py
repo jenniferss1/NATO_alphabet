77ec45ff5666249aa0fa9d18541c8716aa88a30d
jennifer <jennifersantossouza@hotmail.com>
2024-12-02 16:31:43 -0300

    feat: Implementing the dictionary and the phonetic logic

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas
+data = pandas.read_csv('nato_phonetic_alphabet.csv')
+
+result = {row.letter: row.code for index, row in data.iterrows()}
+
+user_input = input("Enter a word: ").upper()
+
+phonetic_code_words = [result[letter] for letter in user_input if letter in result]
+
+print(phonetic_code_words)
